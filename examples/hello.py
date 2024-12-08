ebca118433e255fc86ed999c2de2aca06ad41772
Harshad Sharma <harshad@sharma.io>
2024-12-08 12:16:10 +0530

    Use makeweb.App in hello.

--- examples/hello.py
+++ examples/hello.py
@@ -1,17 +1,13 @@
-import sys
-
-sys.path.append("lib")
-from microdot import Microdot, Response
+from makeweb import App
 
 
-app = Microdot()
+app = App()
 
 
 @app.route("/")
 def index(request):
-    return Response("Hello, World!")
+    return app.response("Hello, World!")
 
 
 if __name__ == "__main__":
-    print("Running on http://localhost:8000")
     app.run(port=8000)
