29f6aeca26dac188c1cd1cb92ac8286e0e319d57
Harshad Sharma <harshad@sharma.io>
2024-12-08 12:24:34 +0530

    Use makeweb.html in hello.py example.

--- examples/hello.py
+++ examples/hello.py
@@ -6,8 +6,14 @@ app = App()
 
 @app.route("/")
 def index(request):
-    return app.response("Hello, World!")
+    doc = app.html(lang="en")
+    with doc:
+        with doc.head():
+            doc.title("MakeWeb")
+        with doc.body():
+            doc.h1("Hello, World!")
+    return app.response(doc)
 
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run()
--- makeweb/app.py
+++ makeweb/app.py
@@ -4,9 +4,17 @@ from makeweb.html import Html, HtmlFragment
 
 class App(Microdot):
     def response(self, content, status_code=200, headers=None):
+        """Create a new response."""
         headers = headers or {"Content-Type": "text/html"}
+        if isinstance(content, Html):
+            content = content.render()
         return Response(content, status_code=status_code, headers=headers)
 
+    def html(self, **attrs):
+        """Create a new HTML document."""
+        return Html(**attrs)
+
     def run(self, host="0.0.0.0", port=8000, debug=False, ssl=None):
+        """Run the application."""
         print(f"Running on http://localhost:{port}")
         super().run(host=host, port=port, debug=debug, ssl=ssl)
