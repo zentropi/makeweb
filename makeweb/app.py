6583655903802b83e0ad463574107acef7eb4a47
Harshad Sharma <harshad@sharma.io>
2024-12-09 11:30:56 +0530

    Allow a more readable configuration of app.

--- makeweb/app.py
+++ makeweb/app.py
@@ -1,8 +1,27 @@
 from microdot import Microdot, Response, send_file, redirect
 from makeweb.html import Html, HtmlFragment
-
+from makeweb.dictdb import DictDB
 
 class App(Microdot):
+    def __init__(self, host="0.0.0.0", port=8000, debug=False, db=None):
+        super().__init__()
+        self.host = host
+        self.port = port
+        self.debug = debug
+        self._db = None
+        self.db = db
+
+    @property
+    def db(self):
+        return self._db
+
+    @db.setter
+    def db(self, value):
+        if isinstance(value, str):
+            self._db = DictDB(value)
+        else:
+            self._db = value
+
     def response(self, content, status_code=200, headers=None, reason=None):
         """Create a new response."""
         headers = headers or {"Content-Type": "text/html"}
@@ -27,8 +46,12 @@ class App(Microdot):
     def html_fragment(self, tag, **attrs):
         """Create a new HTML fragment."""
         return HtmlFragment(tag, **attrs)
-
-    def run(self, host="0.0.0.0", port=8000, debug=False, ssl=None):
+    
+    def run(self, host=None, port=None, debug=None, ssl=None):
         """Run the application."""
-        print(f"Running on http://localhost:{port}")
-        super().run(host=host, port=port, debug=debug, ssl=ssl)
+        run_host = host if host is not None else self.host
+        run_port = port if port is not None else self.port
+        run_debug = debug if debug is not None else self.debug
+        
+        print(f"Running on http://{run_host}:{run_port}")
+        super().run(host=run_host, port=run_port, debug=run_debug, ssl=ssl)
